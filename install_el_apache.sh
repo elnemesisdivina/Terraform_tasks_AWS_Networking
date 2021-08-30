06c82d57eff7b1c7e6c9efc02d99567a2a047609
Raymundo Escobar <rcastaneda@rcastaneda-a02.vmware.com>
2021-08-30 18:08:46 -0500

    adding final task2 and script with last corrections comments on PR

--- install_el_apache.sh
+++ install_el_apache.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-sudo apt-get update
-sudo apt-get install -y apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
-echo "<h1>Web server Task2 with Terraform</h1>" | sudo tee /var/www/html/index.html
+apt-get update
+apt-get install -y apache2
+systemctl start apache2
+systemctl enable apache2
+"<h1>Web server Task2 with Terraform</h1>" | sudo tee /var/www/html/index.html
