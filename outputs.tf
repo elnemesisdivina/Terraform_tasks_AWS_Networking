8eb5946d7601324e7f670fe8edd368a64ebc3dcc
Vladimir Mukhin <awsvlad@gmail.com>
2021-09-06 20:58:11 -0400

    Add vpc module

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+# output "instance_ip_addr" {
+#   value = aws_instance.vray_instance.private_ip
+
+#   description = "The private IP address of the vRay instance."
+# }
+
+# output "jumpbox_ip_addr" {
+#   value = aws_instance.vray_jumpbox.public_ip
+
+#   description = "The public IP address of the vRay Jumpbox."
+# }
